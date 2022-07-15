cb756a669883142fa3578e7c100071e535d9a575
Raj padval <rajpadval145@gmail.com>
2022-07-15 21:57:18 +0530

    second commit

--- db/conn.js
+++ db/conn.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+const validator = require('validator')
 const mongo_uri =
   "mongodb+srv://rajpadval:Raj@123@cluster0.jil81.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongo_uri,()=>{
     console.log(`Connection to the database sucessfull`);
 })
 
-
+mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+  }
+})
